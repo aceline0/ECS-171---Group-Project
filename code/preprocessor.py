29b40aae60b2a53303ba80c1cfb4767cd8af1c8f
Aske <aske.nord.raahauge@gmail.com>
2022-12-04 16:35:41 -0800

    deleted stuff in nb and added seperated preprocessor

---
+++ code/preprocessor.py
@@ -0,0 +1,35 @@
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from matplotlib import pyplot as plt
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import SMOTE
+
+
+df = pd.read_csv("../data/creditcard.csv")
+
+df = df.drop_duplicates()
+df = df.reset_index()
+df = df.drop('index', axis=1)
+
+
+X = df.drop('Class', axis=1)
+y = df['Class']
+
+
+scaler = MinMaxScaler()
+cols = list(df.columns)
+cols.remove('Class')
+
+
+df = pd.DataFrame(scaler.fit_transform(X), columns = cols)
+df = pd.concat([df, y], axis=1)
+df = df.drop(['Time', 'V13', 'V15', 'V22','V23','V24','V25','V26', 'V27','V28','Amount', 'V8', 'V21'], axis=1)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=21)
+smote = SMOTE(random_state=21)
+
+X_train, y_train = smote.fit_resample(X, y)
